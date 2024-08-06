8f458a6eaf7b26ec00a9d5d4ef292b202258c81c
kyungmim <kyungmin727@gmail.com>
2024-08-06 20:49:35 +0900

    feat: heade,footer ui작업 완료

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,44 +1,56 @@
+'use client';
 import Link from 'next/link';
 import styles from './Footer.module.scss';
-import PlantBook from '@images/PlantBook_nor.svg';
-import PlantCommunity from '@images/PlantCommunity_nor.svg';
-import Home from '@images/Home_nor.svg';
-import Diary from '@images/Diary_nor.svg';
-import MyPage from '@images/Mypage_nor.svg';
 import Image from 'next/image';
+import community_nor from '@images/PlantCommunity_nor.svg';
+import community_sel from '@images/PlantCommunity_sel.svg';
+import mypage_nor from '@images/Mypage_nor.svg';
+import mypage_sel from '@images/Mypage_sel.svg';
+import diary_nor from '@images/Diary_nor.svg';
+import diary_sel from '@images/Diary_sel.svg';
+import home_nor from '@images/Home_nor.svg';
+import home_sel from '@images/Home_sel.svg';
+import book_nor from '@images/PlantBook_nor.svg';
+import book_sel from '@images/PlantBook_sel.svg';
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
+  const pathname = usePathname();
+
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.layout_wrapper}>
       <ul className={styles.nav}>
         <li>
           <Link href="/book" className={styles.nav_item}>
-            <Image src={PlantBook} alt="식물 백과" width={16} height={16} />
-            <p>식물 백과</p>
+            {pathname === '/book' ? <Image src={book_sel} alt="식물 백과" width={16} height={16} /> : <Image src={book_nor} alt="식물 백과" width={16} height={16} />}
+
+            <p className={`${pathname === '/book' ? `${styles.is_active}` : ''}`}>식물 백과</p>
           </Link>
         </li>
         <li>
           <Link href="/community" className={styles.nav_item}>
-            <Image src={PlantCommunity} alt="식물 이야기" width={16} height={16} />
-            <p>식물 이야기</p>
+            {pathname === '/community' ? <Image src={community_sel} alt="식물 이야기" width={16} height={16} /> : <Image src={community_nor} alt="식물 이야기" width={16} height={16} />}
+            <p className={`${pathname === '/community' ? `${styles.is_active}` : ''}`}>식물 이야기</p>
           </Link>
         </li>
         <li>
           <Link href="/" className={styles.nav_item}>
-            <Image src={Home} alt="메인페이지" width={16} height={16} />
-            <p>홈</p>
+            {pathname === '/' ? <Image src={home_sel} alt="홈" width={16} height={16} /> : <Image src={home_nor} alt="홈" width={16} height={16} />}
+
+            <p className={`${pathname === '/' ? `${styles.is_active}` : ''}`}>홈</p>
           </Link>
         </li>
         <li>
           <Link href="/myplant" className={styles.nav_item}>
-            <Image src={Diary} alt="나의 식물" width={16} height={16} />
-            <p>나의 식물</p>
+            {pathname === '/myplant' ? <Image src={diary_sel} alt="나의 식물" width={16} height={16} /> : <Image src={diary_nor} alt="나의 식물" width={16} height={16} />}
+
+            <p className={`${pathname === '/myplant' ? `${styles.is_active}` : ''}`}>나의 식물</p>
           </Link>
         </li>
         <li>
           <Link href="/profile" className={styles.nav_item}>
-            <Image src={MyPage} alt="마이페이지" width={16} height={16} />
-            <p>마이페이지</p>
+            {pathname === '/profile' ? <Image src={mypage_sel} alt="마이페이지" width={16} height={16} /> : <Image src={mypage_nor} alt="마이페이지" width={16} height={16} />}
+            <p className={`${pathname === '/profile' ? `${styles.is_active}` : ''}`}>마이페이지</p>
           </Link>
         </li>
       </ul>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,14 @@
 import styles from './Header.module.scss';
+import logo from '@images/Logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
-  return <h1>헤더</h1>;
+  return (
+    <header className={styles.layout_wrapper}>
+      <Link href="/" className={styles.logo}>
+        <Image src={logo} alt="greeny" width={100} height={30} />
+      </Link>
+    </header>
+  );
 }
