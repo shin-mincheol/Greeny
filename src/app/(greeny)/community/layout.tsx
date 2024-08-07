057f3ffa745ac7aadbe36f01fb060573146afa95
mincheol.shin <shinmch22@naver.com>
2024-08-07 15:37:18 +0900

    cont: 커뮤니티 UI 작업

--- src/app/(greeny)/community/layout.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './Community.module.scss';
-
-export default function layout({ children }: { children: React.ReactNode }) {
-  return <main className={styles.container}>{children}</main>;
-}
