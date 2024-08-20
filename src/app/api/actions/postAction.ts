eff8d0964512d7bb85f90179e8af834f2cc2d349
mincheol.shin <shinmch22@naver.com>
2024-08-20 11:57:23 +0900

    cont: 게시글 댓글 수정/삭제 기능 #29

--- src/app/api/actions/postAction.ts
+++ src/app/api/actions/postAction.ts
@@ -2,7 +2,7 @@
 
 import { auth } from '@/auth';
 import { PostComment } from '@/types/post';
-import { CoreErrorRes, SingleItem } from '@/types/response';
+import { CoreErrorRes, CoreSuccessRes, SingleItem } from '@/types/response';
 import { revalidatePath } from 'next/cache';
 
 const SERVER = process.env.NEXT_PUBLIC_API_SERVER;
@@ -23,6 +23,46 @@ export async function addReply(postId: string, formData: FormData): Promise<Sing
         Authorization: `Bearer ${session?.accessToken}`,
       },
       body: JSON.stringify(data),
+    });
+    revalidatePath(`/story/community/${postId}`);
+    return res.json();
+  } catch (error) {
+    throw new Error('network error');
+  }
+}
+
+export async function updateReply(postId: string, replyId: number, formData: FormData): Promise<SingleItem<PostComment> | CoreErrorRes> {
+  const session = await auth();
+  const data = {
+    content: formData.get('content'),
+  };
+
+  try {
+    const res = await fetch(`${SERVER}/posts/${postId}/replies/${replyId}`, {
+      method: 'PATCH',
+      headers: {
+        'client-id': `${DBNAME}`,
+        'Content-Type': 'application/json',
+        Authorization: `Beareㄹr ${session?.accessToken}`,
+      },
+      body: JSON.stringify(data),
+    });
+    revalidatePath(`/story/community/${postId}`);
+    return res.json();
+  } catch (error) {
+    throw new Error('network error');
+  }
+}
+
+export async function deleteReply(postId: string, replyId: number): Promise<CoreSuccessRes | CoreErrorRes> {
+  const session = await auth();
+  try {
+    const res = await fetch(`${SERVER}/posts/${postId}/replies/${replyId}`, {
+      method: 'DELETE',
+      headers: {
+        'client-id': `${DBNAME}`,
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
     });
     revalidatePath(`/story/community/${postId}`);
     return res.json();
