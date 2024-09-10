2f52aa833493bfbbfd85d0199ceac672852e6e93
kyungmim <kyungmin727@gmail.com>
2024-09-10 18:05:25 +0900

    style: header,footer,메인페이지 반응형 작업 완료

--- src/app/(greeny)/(home)/(section)/Banner.tsx
+++ src/app/(greeny)/(home)/(section)/Banner.tsx
@@ -4,21 +4,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Image from 'next/image';
 import bannerIcon from '@images/Banner_Icon.svg';
-import banner1 from '@images/BannerImage1.png';
-import banner2 from '@images/BannerImage2.png';
-import banner3 from '@images/BannerImage3.png';
-import banner4 from '@images/BannerImage4.png';
 import Link from 'next/link';
 import { Autoplay } from 'swiper/modules';
 
 export default function Banner() {
   return (
-    <Swiper className={styles.bannerList} slidesPerView={1} spaceBetween={0} modules={[Autoplay]} autoplay={{ delay: 5000, disableOnInteraction: false }}>
+    <Swiper className={styles.bannerList} loop={true} slidesPerView={1} spaceBetween={0} modules={[Autoplay]} autoplay={{ delay: 5000, disableOnInteraction: false }}>
       <SwiperSlide className={styles.banner_item}>
-        <div className={styles.banner_cover}>
-          <Image src={banner3} alt="식물 사진" fill sizes="100%" />
-        </div>
-
         <div className={styles.banner_info}>
           <div className={styles.banner_text}>
             <span>식물 친구 추천해요!</span>
@@ -28,17 +20,17 @@ export default function Banner() {
             </p>
           </div>
 
-          {/* <Link href="/event" className={styles.bannerLink}>
-            <Image src={bannerIcon} width={40} height={40} alt="테스트하러가기" />
-          </Link> */}
+          <Link href="/event" className={styles.bannerLink}>
+            <div className={styles.eventPc}>
+              <p>바로가기</p>
+              <Image src={bannerIcon} width={20} height={20} alt="테스트하러가기" />
+            </div>
+            <Image className={styles.eventMo} src={bannerIcon} width={40} height={40} alt="테스트하러가기" />
+          </Link>
         </div>
       </SwiperSlide>
 
       <SwiperSlide className={styles.banner_item}>
-        <div className={styles.banner_cover}>
-          <Image src={banner2} alt="식물 사진" fill sizes="100%" />
-        </div>
-
         <div className={styles.banner_info}>
           <div className={styles.banner_text}>
             <span>오늘의 식물 추천!</span>
@@ -48,17 +40,17 @@ export default function Banner() {
             </p>
           </div>
 
-          {/* <Link href="/event" className={styles.bannerLink}>
-            <Image src={bannerIcon} width={50} height={50} alt="테스트하러가기" />
-          </Link> */}
+          <Link href="/event" className={styles.bannerLink}>
+            <div className={styles.eventPc}>
+              <p>바로가기</p>
+              <Image src={bannerIcon} width={20} height={20} alt="테스트하러가기" />
+            </div>
+            <Image className={styles.eventMo} src={bannerIcon} width={40} height={40} alt="테스트하러가기" />
+          </Link>
         </div>
       </SwiperSlide>
 
       <SwiperSlide className={styles.banner_item}>
-        <div className={styles.banner_cover}>
-          <Image src={banner1} alt="식물 사진" fill sizes="100%" />
-        </div>
-
         <div className={styles.banner_info}>
           <div className={styles.banner_text}>
             <span>식물 친구 추천해요!</span>
@@ -69,17 +61,17 @@ export default function Banner() {
             </p>
           </div>
 
-          {/* <Link href="/event" className={styles.bannerLink}>
-            <Image src={bannerIcon} width={50} height={50} alt="테스트하러가기" />
-          </Link> */}
+          <Link href="/event" className={styles.bannerLink}>
+            <div className={styles.eventPc}>
+              <p>바로가기</p>
+              <Image src={bannerIcon} width={20} height={20} alt="테스트하러가기" />
+            </div>
+            <Image className={styles.eventMo} src={bannerIcon} width={40} height={40} alt="테스트하러가기" />
+          </Link>
         </div>
       </SwiperSlide>
 
       <SwiperSlide className={styles.banner_item}>
-        <div className={styles.banner_cover}>
-          <Image src={banner4} alt="식물 사진" fill sizes="100%" />
-        </div>
-
         <div className={styles.banner_info}>
           <div className={styles.banner_text}>
             <span>식물 맞춤 추천!</span>
@@ -89,9 +81,13 @@ export default function Banner() {
             </p>
           </div>
 
-          {/* <Link href="/event" className={styles.bannerLink}>
-            <Image src={bannerIcon} width={50} height={50} alt="테스트하러가기" />
-          </Link> */}
+          <Link href="/event" className={styles.bannerLink}>
+            <div className={styles.eventPc}>
+              <p>바로가기</p>
+              <Image src={bannerIcon} width={20} height={20} alt="테스트하러가기" />
+            </div>
+            <Image className={styles.eventMo} src={bannerIcon} width={40} height={40} alt="테스트하러가기" />
+          </Link>
         </div>
       </SwiperSlide>
     </Swiper>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,52 @@
+import { auth } from '@/auth';
 import styles from './Header.module.scss';
 import logo from '@images/Logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
   return (
-    <header className={styles.layout_wrapper}>
-      <Link href="/" className={styles.logo}>
-        <Image src={logo} alt="greeny" width={100} height={30} priority />
-      </Link>
-    </header>
+    <>
+      <header className={styles.headPc_wrapper}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logo} alt="greeny" width={100} height={30} priority />
+        </Link>
+
+        <ul className={styles.nav}>
+          <li>
+            <Link href="/books" className={styles.nav_item}>
+              식물 백과
+            </Link>
+          </li>
+          <li>
+            <Link href="/story" className={styles.nav_item}>
+              식물 이야기
+            </Link>
+          </li>
+          <li>
+            <Link href="/plant" className={styles.nav_item}>
+              나의 식물
+            </Link>
+          </li>
+          <li>
+            {session?.user ? (
+              <Link href="/profile" className={styles.nav_item}>
+                마이페이지
+              </Link>
+            ) : (
+              <Link href="/login" className={`${styles.nav_item} ${styles.login}`}>
+                로그인
+              </Link>
+            )}
+          </li>
+        </ul>
+      </header>
+      <header className={styles.headMo_wrapper}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logo} alt="greeny" width={100} height={30} priority />
+        </Link>
+      </header>
+    </>
   );
 }
