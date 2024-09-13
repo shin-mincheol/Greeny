04e510dfbe754e126c280be9c40b266995b228bc
no-support <tk2ektk3ek@gmail.com>
2024-09-13 15:25:02 +0900

    cont: 주석 제거 및 include mobile 작업 중

--- src/app/(greeny)/profile/[_id]/DeleteButton.tsx
+++ src/app/(greeny)/profile/[_id]/DeleteButton.tsx
@@ -2,7 +2,6 @@
 import { useTransition } from 'react';
 import { deleteBookmark } from '@/app/api/actions/followAction';
 import Button from '@/components/button/Button';
-import { redirect } from 'next/navigation';
 
 export default function DeleteButton({ _id, children }: { _id: number; children: React.ReactNode }) {
   const [isPending, startTransition] = useTransition();
@@ -19,28 +18,6 @@ export default function DeleteButton({ _id, children }: { _id: number; children:
       }
     });
   };
-
-  /*
-  .followBtn {
-    padding: 0.6rem 1.7rem;
-    background-color: #a7c4a0;
-    border: 0.1rem solid #a7c4a0;
-    border-radius: 0.6rem;
-    color: var(--color-white);
-    font-size: 1rem;
-  }
-  .followBtn.selected {
-    background-color: var(--color-white);
-    color: #a7c4a0;
-  }
-  */
-
-  // const style = {
-  //   padding: '0.4rem 1rem',
-  //   borderRadius: '1rem',
-  //   border: isPending ? 'var(--color-primary-disabled)' : 'var(--color-primary)',
-  //   backgroundColor: isPending ? 'var(--color-primary-disabled)' : 'var(--color-primary)',
-  // };
 
   const style = {
     padding: ' 0.6rem 1.7rem',
