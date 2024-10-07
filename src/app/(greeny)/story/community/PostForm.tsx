125c6755f9e44514a9e29064d29a5df5135ab240
mincheol.shin <shinmch22@naver.com>
2024-10-07 20:45:27 +0900

    refactor: 게시글 작성/수정 react-hook-form 적용 #106

--- src/app/(greeny)/story/community/PostForm.tsx
+++ src/app/(greeny)/story/community/PostForm.tsx
@@ -6,13 +6,16 @@ import PostCategory from '@greeny/story/community/PostCategory';
 import PostContent from '@greeny/story/community/PostContent';
 import PostImage from '@greeny/story/community/PostImage';
 import { useState } from 'react';
-import { PostRes } from '@/types/post';
+import { type PostForm, PostRes } from '@/types/post';
 import { ImageRes } from '@/types/image';
 import { addPost, updatePost } from '@/app/api/actions/postAction';
+import { useForm } from 'react-hook-form';
 
 type Props = {
   post?: PostRes;
 };
+
+export type Form = Omit<PostForm, 'type'> & { attach: File[] };
 
 const categories: { name: 'free' | 'planterior' | 'qna'; value: '자유' | '플랜테리어' | '질문' }[] = [
   { name: 'free', value: '자유' },
@@ -21,14 +24,45 @@ const categories: { name: 'free' | 'planterior' | 'qna'; value: '자유' | '플�
 ];
 
 export default function PostForm({ post }: Props) {
+  const defaultValues = {
+    title: post?.title ?? '',
+    content: post?.content ?? '',
+    category: post?.extra?.category ?? categories[0].name,
+    attach: [],
+  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<Form>({ defaultValues });
+  const selectedCategory = watch('category');
   const [originalImage, setOriginalImage] = useState<ImageRes[]>(post?.image ?? []);
-  const update = updatePost.bind(null, post?._id!, originalImage);
-
+  const update = (form: Form) => {
+    const formData = new FormData();
+    formData.append('title', form.title);
+    formData.append('content', form.content);
+    formData.append('category', form.category);
+    form.attach.forEach((file) => formData.append('attach', file));
+    updatePost.bind(null, post?._id!, originalImage)(formData);
+  };
+  const add = (form: Form) => {
+    const formData = new FormData();
+    formData.append('title', form.title);
+    formData.append('content', form.content);
+    formData.append('category', form.category);
+    form.attach.forEach((file) => formData.append('attach', file));
+    addPost(formData);
+  };
+  const setFile = (files: File[]) => {
+    setValue('attach', files);
+  };
   return (
-    <form action={post ? update : addPost} className={postStyles.post_form}>
-      <PostImage originalImage={originalImage} setOriginalImage={setOriginalImage} />
-      <PostCategory categoryList={categories} initialCategory={post?.extra?.category} />
-      <PostContent post={post && { title: post.title, content: post.content }} />
+    <form onSubmit={handleSubmit(post ? update : add)} className={postStyles.post_form}>
+      <PostImage register={register} originalImage={originalImage} setOriginalImage={setOriginalImage} setFile={setFile} />
+      <PostCategory register={register} categoryList={categories} selected={selectedCategory} />
+      <PostContent register={register} errors={errors} />
       <button type="submit" className={postStyles.btn_submit}>
         {post ? '수정하기' : '등록하기'}
       </button>
